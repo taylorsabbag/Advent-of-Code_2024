0f81898daab9c2ed3cfee91783671ee600311103
Taylor Sabbag <taylorsabbag@gmail.com>
2024-12-03 15:35:01 -0500

    chore: rename decorators to performance.

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ export * from "@utils/checkAnswer.js";
 export * from "@utils/runner.js";
 export * from "@utils/dates.js";
 export * from "@utils/cache.js";
-export * from "@utils/decorators.js";
+export * from "@utils/performance.js";
 export * from "@utils/errors.js";
 
 export { default as checkAnswer } from "@utils/checkAnswer.js";
@@ -11,5 +11,5 @@ export { default as runner } from "@utils/runner.js";
 export { extractDayNumber, getCurrentYear } from "@utils/dates.js";
 export { getCachedInput, cacheInput } from "@utils/cache.js";
 export { default as getInput } from "@utils/getInput.js";
-export { default as timed } from "@utils/decorators.js";
+export { default as timed } from "@utils/performance.js";
 export { default as AoCError } from "@utils/errors.js";
